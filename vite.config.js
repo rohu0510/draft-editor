73aef1b797972b153b4df70a4265f8d18561167e
Rohit Prajapati <65508466+rohu0510@users.noreply.github.com>
2025-02-03 17:12:34 +0530

    host add and config update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Allow access from any network
-    port: 5173, // Use the default Vite port
-    strictPort: false, // Prevent Vite from changing the port
-  },
-  preview: {
-    port: 4173, // Default preview mode port
-    strictPort: false,
-  },
-});
+})
